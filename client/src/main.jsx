4c3b6c36c156453a5f659b9ad621af9afd27ffbc
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-24 21:34:44 +0530

    Update client/src/main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './theme.css'
+
+document.documentElement.dataset.theme =
+  localStorage.getItem('artifact-theme') === 'light'
+    ? 'light'
+    : 'dark'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
