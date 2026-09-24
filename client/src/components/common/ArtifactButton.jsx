4a8cc4550d59d9fbfaf030f1076a95efa522615b
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-24 20:37:28 +0530

    Update client/src/components/common/ArtifactButton.jsx

---
+++ client/src/components/common/ArtifactButton.jsx
@@ -0,0 +1,18 @@
+export default function Button({
+  children,
+  type = "button",
+  onClick,
+  disabled = false,
+  className = "",
+}) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`artifact-button ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
